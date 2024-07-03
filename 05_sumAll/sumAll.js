1b0d9e37bd22603ac970a78f8784c9a357718d5a
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2024-07-03 14:29:35 -0600

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (
+        (typeof(num1) === 'number' && typeof(num2) === 'number') && 
+        (num1 >= 0 && num2 >= 0) 
+    ) {
+        sum = 0;
+
+        // Reverse so the bigger number is always num2
+        if (num1 > num2) {
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        for (let i = num1; i <= num2; i++) {
+            sum = sum + i;
+        }
+
+        return sum;
+
+    } else {
+        return 'ERROR';
+    }
+ 
 };
 
 // Do not edit below this line
