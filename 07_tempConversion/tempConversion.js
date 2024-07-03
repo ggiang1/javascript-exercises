b510b7dd14d9985f593567c6b867fbfb99a92c88
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2024-07-03 14:44:10 -0600

    Completed tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  var celsius = (temperature - 32) * (5/9);
+  var celsius = Math.round(celsius * 10) / 10;
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  var fahrenheit = (temperature * 9/5) + 32;
+  var fahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  return fahrenheit;
+
 };
 
 // Do not edit below this line
