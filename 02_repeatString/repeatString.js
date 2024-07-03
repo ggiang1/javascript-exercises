9b6fce125db2f1bdedac75de42fdb84123cac94a
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2024-07-03 14:02:02 -0600

    Completed 03_reverseString
    Added semi colons for proper practice.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,10 @@
 const repeatString = function(text, amount) {
     // return text.repeat(amount)
-    var final_string = ''
+    var final_string = '';
     for (let i = 0; i < amount; i++) {
-        final_string += text
+        final_string += text;
     }
-    return final_string
+    return final_string;
 };
 
 // Do not edit below this line
