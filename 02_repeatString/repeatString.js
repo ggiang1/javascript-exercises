87a635dedc61e0ffb9026010e26016f3935a2389
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2024-07-03 13:59:26 -0600

    Finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, amount) {
+    // return text.repeat(amount)
+    var final_string = ''
+    for (let i = 0; i < amount; i++) {
+        final_string += text
+    }
+    return final_string
 };
 
 // Do not edit below this line
