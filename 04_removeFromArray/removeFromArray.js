6154dc0bab203abf6821e41e5e1431f1807b8641
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2024-07-03 14:23:45 -0600

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    // for (let num of args) {
+    //     for (i = 0; i < array.length; i++) {
+    //         const index = array.indexOf(num);
+    //         if (index >-1) {
+    //             array.splice(index, 1);
+    //         }
+    //     }
+    // }
+    // return array;
+    return array.filter(num => !args.includes(num)); // Keep every number that is in array but not in args.
 };
 
 // Do not edit below this line
